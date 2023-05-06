0ff36b029eb19cddca11e29d9340b921e2240941
Mehedi Hasan <mehedih.dev@gmail.com>
2023-05-06 21:23:09 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "./Components/Card";
+import contacts from "./contacts"
+
+const App=()=>{
+    return(
+        <div>
+           <h1 className="heading">Contact App</h1>
+           <Card name={contacts[0].name} 
+           img='https://upload.wikimedia.org/wikipedia/en/b/bd/Doraemon_character.png'
+           email='blueDora@yahoo.com'
+           phone='+8446544564'
+            />
+           <Card
+            name='Red Dora'
+            img='https://i.pinimg.com/originals/48/df/e5/48dfe5cc7740cd2ba81e6f3e31ff2d28.jpg'
+            email='redDora@icloud.com'
+            phone='+94654464'
+
+           />
+           <Card
+            name='Green Dora'
+            img='https://i.pinimg.com/550x/1f/53/4e/1f534ef919fa315b00ad12ac04353d9d.jpg'
+            email='greenDora@gmail.com'
+            phone="+84554544"
+           />
+        </div>
+    )
+}
+export default App
